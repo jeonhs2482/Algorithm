870c55330ba83497c3acc2198c3f046e82d06071
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2025-03-09 15:40:08 +0900

    프로그래머스 - 대소문자 바꿔서 출력하기 문제 풀이

---
+++ Programmers/Level0/Solution004.java
@@ -0,0 +1,29 @@
+package Level0;
+import java.util.Scanner;
+
+/*
+- 문제 설명 -
+영어 알파벳으로 이루어진 문자열 str이 주어집니다.
+각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+*/
+
+public class Solution004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                System.out.print(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                System.out.print(Character.toUpperCase(c));
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
